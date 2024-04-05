60e3b4485217b9866d081517dd8b3307a637d64c
Jamie Theo Junk <83469722+st10084689@users.noreply.github.com>
2024-04-05 11:38:19 +0200

    Update TrackModel.kt

--- app/src/main/java/com/harmless/bmiproject/models/TrackModel.kt
+++ app/src/main/java/com/harmless/bmiproject/models/TrackModel.kt
@@ -2,7 +2,7 @@ package com.harmless.bmiproject.models
 
 import java.util.Date
 
-data class TrackModel(
+data class TrackModel(//model to track the bmi the user has received and store it in the cloud 
     val weight: String,
     val bmi: Double,
     val date: Date?,
